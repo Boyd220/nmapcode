5549ca99b35edf377adbf267837f88d7dc7e2997
root <root@raspberrypi>
2021-05-21 11:37:20 +0200

    first commit

---
+++ nmapscript.sh
@@ -0,0 +1,28 @@
+ #!/bin/sh
+
+echo this is a test > testoutput1.txt
+
+default_if=$(ip route list | awk '/^default/ {print $5}')
+
+iptoscan=$(ip -o -f inet addr show $default_if | awk '{print $4}')
+
+networkmask=$(cut -d "/" -f2 <<< "$iptoscan")
+
+originalnetworkmask=$(ifconfig eth0 | awk '/netmask/{split($4,a,":"); print a[1]}')
+
+currentip=$(hostname -I | awk '{print $1}')
+
+IFS=. read -r i1 i2 i3 i4 <<< "$currentip"
+IFS=. read -r m1 m2 m3 m4 <<< "$originalnetworkmask"
+
+networkaddress=$((i1 & m1)).$((i2 & m2)).$((i3 & m3)).$((i4 & m4))
+
+echo $networkaddress
+
+scanthis=$networkaddress/$networkmask
+
+echo $scanthis
+
+sudo nmap $scanthis -sn -oG nmap-list.txt
+
+sudo python newmailing.py 
